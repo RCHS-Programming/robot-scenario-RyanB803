6014fa00aa8671a6c23a00590fb35ad9cbb76b87
RyanB803 <75798925+RyanB803@users.noreply.github.com>
2021-04-02 10:32:30 -0400

    Final robot scenario

--- Robot.java
+++ Robot.java
@@ -8,6 +8,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Robot extends Actor
 {
+    private GreenfootImage image1;
+    private GreenfootImage image2;
+    private GreenfootImage image3;
+    public Robot()
+    {
+        image1=new GreenfootImage("man01.png");
+        image2=new GreenfootImage("man02.png");
+        setImage(image1);
+        image3= new GreenfootImage("gameover.png");
+    }
     /**
      * Act - do whatever the Robot wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -17,6 +27,11 @@ public class Robot extends Actor
         robotMovement();
         detectWallCollision();
         detectBlockCollision();
+        eatPizza();
+        animate();
+        PizzaEaten();
+        removeLive();
+        
     }   
     
     //robot movement keys 
@@ -46,6 +61,8 @@ public class Robot extends Actor
         if(isTouching(Wall.class))
         {
             setLocation(29,49);
+            Greenfoot.playSound("hurt.wav");
+            
         }
     }
     
@@ -55,6 +72,82 @@ public class Robot extends Actor
         if(isTouching(Block.class))
         {
             setLocation(29,49);
+            Greenfoot.playSound("hurt.wav");
+            
         }
     }
+    
+    //sends you back to the start 
+    //score increesed every time robot goes home
+    public void detectHome()
+    {
+        if(isTouching(Home.class))
+        {
+                Greenfoot.playSound("yipee.wav");
+                setLocation(29,49);
+                score++;
+                pizzaEaten=0;
+        }
+    }
+    
+    //check if pizza is being touched 
+    public void eatPizza()
+    {
+        if (isTouching(Pizza.class))
+        {
+            Greenfoot.playSound("eat.wav");
+            removeTouching(Pizza.class); 
+            pizzaEaten++;
+        }
+    }
+       
+    public void animate()
+    {
+        if (getImage()==image1)
+        {
+            setImage(image2);
+        }
+        else 
+        {
+            setImage(image1);
+        }
+        robotMovement();
+    }
+    
+    //tells how many pizzas are eaten 
+    public void PizzaEaten()
+    {
+        if(pizzaEaten==5)
+        {
+            detectHome();
+            
+        }
+    }
+    
+    //removes a life if touches wall or block 
+    public void removeLive()
+    {
+        if (isTouching(Wall.class))
+        {
+            Lives = Lives-1;
+        }
+        if(isTouching(Block.class))
+        {
+            Lives = Lives-1;
+        }
+        if(Lives==0)
+        {
+            Greenfoot.stop();
+            setImage(image3);
+        }
+    }
+    
+    //score isincreesed every time robot goes home 
+   
+    
+    private int Lives = 3;
+    private int score=0; 
+    private int pizzaEaten=0;   
+    
 }
+  
